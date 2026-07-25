adb264ebd500c15dffc69a8c57ca7a8ded3840a3
Buse Koroglu <buse.krgl20@gmail.com>
2026-07-25 15:37:18 +0300

    implemented ApplicationDbContext skeleton with DbSets

---
+++ ChefPos.Infastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+using ChefPos.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChefPos.Infastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options){}
+    
+    public DbSet<User> Users => Set<User>();
+    public DbSet<UserLocation> UserLocations => Set<UserLocation>();
+    public DbSet<Location> Locations => Set<Location>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+    public DbSet<ProductItem>  ProductItems => Set<ProductItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
